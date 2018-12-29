8b7bbc31a55226a3b94840bba8407fe97a1bf300
Ahmed Hadjou <thehidjou@yahoo.com>
2018-12-29 15:49:02 +0000

    created tag pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,6 +21,7 @@ exports.createPages = ({ actions, graphql }) => {
   const templates = {
     singlePost: path.resolve('src/templates/single-post.js'),
     tagsPage: path.resolve('src/templates/tags-page.js'),
+    tagPosts: path.resolve('src/templates/tag-posts.js'),
   }
 
   return graphql(`
@@ -87,6 +88,17 @@ exports.createPages = ({ actions, graphql }) => {
         tags,
         tagPostCounts,
       },
+    })
+
+    // Create tag posts pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tag/${slugify(tag)}`,
+        component: templates.tagPosts,
+        context: {
+          tag,
+        },
+      })
     })
   })
 }
---
+++ src/templates/tag-posts.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Post from '../components/Post'
+
+const tagPosts = ({ data, pageContext }) => {
+  const { tag } = pageContext
+  const { totalCount } = data.allMarkdownRemark
+  const pageHeader = `${totalCount} post${
+    totalCount === 1 ? '' : 's'
+  } tagged with "${tag}"`
+
+  return (
+    <Layout pageTitle={pageHeader}>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <Post
+          key={node.id}
+          slug={node.fields.slug}
+          title={node.frontmatter.title}
+          author={node.frontmatter.author}
+          date={node.frontmatter.date}
+          body={node.excerpt}
+          tags={node.frontmatter.tags}
+          fluid={node.frontmatter.image.childImageSharp.fluid}
+        />
+      ))}
+    </Layout>
+  )
+}
+
+export const tagQuery = graphql`
+  query($tag: String!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM Do YYYY")
+            author
+            tags
+            image {
+              childImageSharp {
+                fluid(maxWidth: 650, maxHeight: 371) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default tagPosts
